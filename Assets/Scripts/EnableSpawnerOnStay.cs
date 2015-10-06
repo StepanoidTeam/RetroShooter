1ae382f8472efd5bc7c8d9586145c618a148927e
bobanko <mishko.vladimir@gmail.com>
2015-10-06 18:36:44 +0300

    trying to restore broken files

---
+++ Assets/Scripts/EnableSpawnerOnStay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnableSpawnerOnStay : MonoBehaviour
+{
+
+    void Start()
+    {
+
+
+        //Spawner spawner = GetComponent<Spawner>();
+        //spawner.enabled = false;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        col.GetComponent<Spawner>().enabled = true;
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        Spawner spawner = col.GetComponent<Spawner>();
+
+        if (spawner != null)
+        {
+            spawner.enabled = false;
+        }
+    }
+}
